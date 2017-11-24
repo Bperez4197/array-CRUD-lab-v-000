5d803f28ee6093e360581a735a09a4454b4e0226
Bperez4197 <bryceperez31@gmail.com>
2017-11-24 12:11:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  ["wow","boy","am",1,"girl",2]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
